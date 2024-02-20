479dde497e87f657da10c42f5519e1227db1b61c
GabrielSchroder <bielsittoni@hotmail.com>
2024-02-20 17:56:27 -0300

    Create ex005.js

---
+++ exercícios/ex005.js
@@ -0,0 +1,14 @@
+let idade = 14 
+
+/* if (idade >= 18) {
+    if (idade < 60) {
+        console.log ("Você é um adulto")
+    }  
+} 
+*/
+
+if (idade >= 18 && idade < 69) {
+    console.log ("Você é um adulto")
+}
+
+/* Se usasse o || no lugar do &&, bastaria que uma das variáveis fosse compatíveis com a idade para que o log ativasse. Com o && Tanto o "idade >= 18" quanto o "idade < 69" devem ser compatíveis com a idade, e sendo assim, não ativaria erroneamente a afirmação dentro da log.  
